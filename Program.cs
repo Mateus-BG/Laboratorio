126f0aef22160242168afa0faf061ea6f837df1f
mateus <mateusbuenog@hotmail.com>
2024-03-16 01:13:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿//Crie uma variável chamada idade e atribua a ela o valor 35 e exiba o seu valor.
+int idade = 35;
+Console.WriteLine(idade);
+//Crie uma variável chamada nome e atribua o valor "Maria" e exiba o seu valor.
+string nome = "Maria";
+Console.WriteLine(nome);
+//Crie uma variável chamada altura e atribua a ela o valor 3.45.
+decimal altura = 3.45m;
+Console.WriteLine(altura);
+//Crie uma constante chamada ano e atribua a ela o valor 12 e exiba o seu valor
+const int ano = 12;
+Console.WriteLine(ano);
+//Declare uma variável nota do tipo double como sendo do tipo Nullable type e atribua o
+//valor 7.80 e exiba o seu valor
+double? nota = 7.8; 
+Console.WriteLine(nota);
+//Quais as diferenças entre os tipos por valor e os tipos por referência ?
+
+//O que é um nullable type e qual a sua utilidade ?
+//Pode deixar o valor de uma variavel nulo.
+
+//O que é Camel Case ? Dê um exemplo de sua aplicação.
+//É uma nomeclatura para escrever frases sem espaços ou pontuação.
+//EX: quando esvrevemos o nome de uma variavel qua haja dois nome tipo primeiroNome, a primeira letra é minuscula e as procimas são maiusculas.
+
+//O que é Pascal Case ? Dê um exemplo de sua aplicação
+//Usada para escrever nome composto mais usado para classes de objetos
+//EX: quando vamo atribuir uma classe PrimeiroNome, a primeira letra é maiuscula e as proximas também são maiuculas.
+
+//Declare duas variáveis x e y como sendo do tipo int e atribua a ela os valores 77 e 66 e a
+//seguir imprima o valor da soma de x com y. (Use o operador aritmético + para realizar a
+//operação de soma)
+int x = 77;
+int y = 66;
+Console.WriteLine(x+y);
+
+//Quais os valores padrões dos tipos de dados bool, char, int, double, float, decimal e
+//string.
+//boll - True/False
+//char - armazena um caractere exemplo 'a'
+//int - usado para indicar um numero inteiro
+//double - usado para indicar um numero com virgula ex 2.5
+//float - mesma função do double só que menos precisa ex 2.5f
+//decimal - mesmma função do double só que com mais precisão ex 2.5m
+//string - usado para declaral uma cadeia de caracteres.
